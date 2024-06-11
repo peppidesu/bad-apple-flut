d38e84773187edc2193f60b0912759c831d20ddf
Pepijn Bakker <bakker.pepijn@gmail.com>
2024-06-11 23:46:54 +0200

    better big endian conversion

--- src/pixel.rs
+++ src/pixel.rs
@@ -11,15 +11,13 @@ pub struct Pixel {
 pub fn pixels_to_cmds(pixels: &[Pixel], offset_x: usize, offset_y: usize) -> Vec<u8> {
     let mut result: Vec<u8> = Vec::with_capacity(pixels.len() * 8);
     for pixel in pixels {        
-        let x = pixel.x + offset_x;
-        let y = pixel.y + offset_y;
+        let x = (pixel.x + offset_x) as u16; 
+        let y = (pixel.y + offset_y) as u16;
         
         result.push(0xB0); // PX command
 
-        result.push((x >> 8) as u8);
-        result.push((x & 0xFF) as u8);
-        result.push((y >> 8) as u8);
-        result.push((y & 0xFF) as u8);
+        result.extend(&x.to_be_bytes());
+        result.extend(&y.to_be_bytes());
         
         result.push(pixel.color.r);
         result.push(pixel.color.g);
